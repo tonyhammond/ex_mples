9674cff9eac4a49ccc74d94c6d3a65bef28c9037
Tony Hammond <tony@tonyhammond.org>
2019-04-05 14:02:01 +0100

    Updated example script

--- test_graph/examples/london100.exs
+++ test_graph/examples/london100.exs
@@ -1,16 +1,33 @@
+# This example queries DBpedia for resource dbr:London (first 100 triples)
+# and loads these to a Neo4j instance
+#
+# Usage:
+#
+# % mix run examples/london100.exs
+# terminationStatus = OK
+# triplesLoaded = 100
+#
+# [%{"nodes" => 33, "paths" => 63, "relationships" => 32}]
+
 import TestGraph.RDF.SPARQL.Client, only: [triples_by_uri: 2]
+
+# subject resource to query (dbr:London)
+resource = "http://dbpedia.org/resource/London"
 
 # Bolt connection
 conn = Bolt.Sips.conn()
 
 # return TestGraph.Graph struct for 100 triples (saved to "london100.ttl")
-# SPARQL query is for dbr:London
-graph = (triples_by_uri("http://dbpedia.org/resource/London", 100)
+graph = (triples_by_uri(resource, 100)
 |> RDF.Turtle.write_string!
 |> TestGraph.RDF.write_graph("london100.ttl"))
 
-# use NeoSemantics.import_turtle/2 to import to Neo4j
-IO.inspect NeoSemantics.import_turtle!(conn, graph.uri)
+# use NeoSemantics.import_turtle/2 to import turtle to Neo4j
+[ map ] = NeoSemantics.import_turtle!(conn, graph.uri)
+
+IO.puts "terminationStatus = " <> map["terminationStatus"]
+IO.puts "triplesLoaded = " <> Integer.to_string(map["triplesLoaded"])
+IO.puts ""
 
 # test Neo4j database
 IO.inspect TestGraph.LPG.Cypher.Client.test()
