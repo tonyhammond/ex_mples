bace2ade277cb224771a483d8f9fde5b87c333ed
Tony Hammond <tony@tonyhammond.org>
2019-04-09 19:27:56 +0100

    Updated .iex.exs

--- test_graph/.iex.exs
+++ test_graph/.iex.exs
@@ -2,13 +2,8 @@ import Bolt.Sips, only: [config: 0, conn: 0]
 import TestGraph
 import TestGraph.Utils
 
-alias TestGraph.Graph
-alias TestGraph.Query
 alias TestGraph.LPG.Cypher
 alias TestGraph.RDF.SPARQL
-
-# import Cypher.Client
-# import SPARQL.Client
 
 Application.put_env(:test_graph,
   :sparql_query, TestGraph.RDF.SPARQL.Client.default_sparql_query)
